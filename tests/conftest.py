1c626d53ac6332e75f36b7de50548195309a8a01
WitchDotor <go8nodel@mail.ru>
2026-09-21 12:42:02 +0300

    working 2.4.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,14 @@ from multiprocessing import Process
 from pathlib import Path
 from typing import Any
 
+from pygame.time import Clock
 import pytest
 import pytest_timeout
-from pygame.time import Clock
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(BASE_DIR))
 
-# Hide the pg screen
+# Hide the pygame screen
 os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
 TIMEOUT_ASSERT_MSG = (
