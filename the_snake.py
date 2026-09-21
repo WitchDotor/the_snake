310e9c6552967e5185e449ce4e3074c4c1b368c1
WitchDotor <go8nodel@mail.ru>
2026-09-21 12:20:14 +0300

    working 2.3

--- the_snake.py
+++ the_snake.py
@@ -1,7 +1,8 @@
+import pygame as pg
+
 import random
 import sys
 from typing import Tuple, List
-import pygame as pg
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 SCREEN_CENTER_WIDTH = SCREEN_WIDTH // 2
@@ -56,9 +57,7 @@ class GameObject:
     def draw_rect(self, position: Tuple[int, int] = (0, 0),
                   color: Tuple[int, int, int] = None,
                   with_border=False):
-        """Отрисовывает квадрат на экране. Если color == None,
-        берется color = self.body_color.
-        Необходим для переиспользования в методе draw()"""
+        """Отрисовывает квадрат на экране. Если color == None, берется color = self.body_color. Необходим для переиспользования в методе draw()"""
         if color is None:
             color = self.body_color
         x_pos = float(position[0])
